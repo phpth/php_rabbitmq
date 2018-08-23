040b8aaf1b78cd882623c864ed4a6c1025eff57c
xiakai <xiakai@51cto.com>
2018-08-23 10:00:35 +0800

    update outputinterface name

--- src/Command/ConsumerPcntlCommand.php
+++ src/Command/ConsumerPcntlCommand.php
@@ -4,7 +4,7 @@ namespace Cto\Rabbit\Command;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputIntrface;
+use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Templating\TemplateNameParser;
 use Symfony\Component\Templating\Loader\FilesystemLoader;
