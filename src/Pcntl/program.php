abc9ab0b18aa32c4673cb220b7b433f1dcfb4d7e
xiakai <xiakai@51cto.com>
2018-08-23 13:23:10 +0800

    add line break

--- src/Pcntl/program.php
+++ src/Pcntl/program.php
@@ -1,21 +1,23 @@
-[program:<?= $consumer_name ?>]
-command=<?= $consumer_command ?>
-numprocs=<?= $consumer_num ?>
-process_name=%(program_name)s_349%(process_num)01d
+[program:<?= $consumer_name ?>]<?= PHP_EOL ?>
+command=<?= $consumer_command ?><?= PHP_EOL ?>
+numprocs=<?= $consumer_num ?><?= PHP_EOL ?>
+process_name=%(program_name)s_%(process_num)02d<?= PHP_EOL ?>
 <?php
 if (isset($consumer_out_log)) {
-?>
-stdout_logfile=<?= $consumer_out_log ?>
+    ?>
+stdout_logfile=<?= $consumer_out_log ?><?= PHP_EOL ?>
 <?php
+
 }
 ?>
 <?php
 if (isset($consumer_error_log)) {
     ?>
-stdout_logfile=<?= $consumer_error_log ?>
+stdout_logfile=<?= $consumer_error_log ?><?= PHP_EOL ?>
 <?php
+
 }
 ?>
-autostart=<?= $consumer_autostart ?>
-autorestart=<?= $consumer_autorestart ?>
-startsecs=<?= $consumer_startsecs ?>
+autostart=<?= $consumer_autostart ?><?= PHP_EOL ?>
+autorestart=<?= $consumer_autorestart ?><?= PHP_EOL ?>
+startsecs=<?= $consumer_startsecs ?><?= PHP_EOL ?>
